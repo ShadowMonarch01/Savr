68b46071d49a7123a91ab49871e812727d860484
ShadowMonarch01 <gunngptrust@gmail.com>
2023-06-30 02:19:22 +0100

    basics complete

--- pricescript.js
+++ pricescript.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const nav_button = document.getElementsByClassName('top-btn-wrapper')[0];
 
     const price_link_pg = document.getElementsByClassName('price-link-pg')[0];
-    // const price_link_pg1 = document.getElementsByClassName('price-link-pg')[1];
+    const price_link_pg1 = document.getElementsByClassName('price-link-pg')[1];
 
     const v_btn = document.getElementsByClassName('tinactive')[0];
 
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         console.log('Hi')
         nav_button.classList.toggle('active')
         price_link_pg.classList.toggle('active')
-        // price_link_pg1.classList.toggle('active')
+        price_link_pg1.classList.toggle('active')
         v_btn.classList.toggle('active')
         x_btn.classList.toggle('active')
     });
