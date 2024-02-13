a95f83befb03a578c6385ec7074c64d8c36fb949
ShadowMonarch01 <gunngptrust@gmail.com>
2024-02-13 08:36:50 +0100

    faqs added

---
+++ faqscript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const faqs = document.querySelectorAll(".faq");
+
+    faqs.forEach(faq =>{
+        faq.addEventListener("click", ()=>{
+            faq.classList.toggle("active")
+        })
+    })
+})
